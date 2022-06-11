3f3da0207502fe062318de7d5fea59ed8cd7421f
ahmetemincoban <ezcrashae@gmail.com>
2022-06-11 09:46:55 +0300

    Initial Commit

---
+++ 01_helloworld/main.go
@@ -0,0 +1,11 @@
+//Package clause
+package main
+
+//Import statement
+import "fmt" //---> Package fmt implements formatted I/O with functions analogous to C's printf and scanf. The format 'verbs' are derived from C's but are simpler.
+
+//My Code
+func main()  {
+	fmt.Print("Hello World")
+	
+}
